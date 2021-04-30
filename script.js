703197423abe072bfc241264bba1ac6678c73e8d
Savannah Kolb <savannahnkolb@gmail.com>
2021-04-29 22:52:28 -0400

    local storage saving, but I am having quotation bug

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
+var saveBtn = $('.row').children('.saveBtn'); 
+
 $(document).ready(function () {
+    
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
-    $(".savebtn").on("click", function () {
-        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
-        console.log(this);
-        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
-
-        //set items in local storage.
-        localStorage.setItem(time, text);
+    
+    saveBtn.on("click", function () {
+        // console.log('It Clicked!')
+        var text = $(this).siblings(".description").val().trim(); // savebtn > textarea > class=description > val collect the user input
+        console.log('text:', text)
+        var time = $(this).parent().attr("id"); // savebtn > div row > attr is id=hour**
+        // $(this) is better to use because it does not run DOM query - refers back to original element
+        // console.log('time:', time)
+        
+        localStorage.setItem(time, JSON.stringify(text)); // save time and text to local storage
     })
-
+  
+    // get the item from hour id and then description
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    // console.log('localStorage.getItem("hour17":', localStorage.getItem("hour17"))
+    
+    
 })
 
+//     // Button for clear the day
+// var clearDay = $(#clearDay);
+//     $("#clearDay").on("click", function(){
+//       localStorage.clear();
+//       initPage()
+//     }) 
 // var currentTimeEl = $('#currentDay');
 
 // function displayTime() {
@@ -62,18 +87,5 @@ $(document).ready(function () {
 //     background()
   
 //     // Buttons (save to Local Storage)
-//     $(".saveBtn").on("click", function(){
-//       userInput = $(this).siblings(".form-control").val().trim();
-//       console.log(userInput);
-//       hourSpan = $(this).siblings(".input-group-prepend").text().trim();
-//       console.log(hourSpan);
-//       localStorage.setItem(hourSpan, JSON.stringify(userInput));
   
-//     })
-//     // Button for clear the day
-//     $("#clearDay").on("click", function(){
-//       localStorage.clear();
-//       initPage()
-//     }) 
-  
-//   });
+//   })
